f6e1196d93dc77233c85138735d295f661aef8af
Bek Brace <60483846+BekBrace@users.noreply.github.com>
2021-01-05 13:50:09 +0100

    Remove Secret Key from Settings.py

--- newsProject/newsProject/settings.py
+++ newsProject/newsProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wznbh@)&z8!4b)hm*%1oy-_*_!hdkg6u@_l1ncjy$0cvv&3==^'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
